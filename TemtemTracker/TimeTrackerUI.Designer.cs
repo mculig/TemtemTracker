a329f1cf48fea6bc6d649c9817a6d5ea2a5e1b4e
Mislav Culig <mculig@gmail.com>
2020-05-02 15:00:56 +0200

    Fix to label sizes in TimeTrackerUI

--- TemtemTracker/TimeTrackerUI.Designer.cs
+++ TemtemTracker/TimeTrackerUI.Designer.cs
@@ -47,62 +47,72 @@
             this.tableLayoutPanel1.Controls.Add(this.labelTimeTitle, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(236, 77);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(157, 50);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // temtemHLabel
             // 
-            this.temtemHLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.temtemHLabel.AutoSize = true;
-            this.temtemHLabel.Location = new System.Drawing.Point(157, 47);
+            this.temtemHLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.temtemHLabel.Location = new System.Drawing.Point(80, 25);
+            this.temtemHLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.temtemHLabel.Name = "temtemHLabel";
-            this.temtemHLabel.Size = new System.Drawing.Size(40, 20);
+            this.temtemHLabel.Size = new System.Drawing.Size(75, 25);
             this.temtemHLabel.TabIndex = 3;
             this.temtemHLabel.Text = "0.00";
+            this.temtemHLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timeLabel
             // 
-            this.timeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.timeLabel.AutoSize = true;
-            this.timeLabel.Location = new System.Drawing.Point(23, 47);
+            this.timeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.timeLabel.Location = new System.Drawing.Point(2, 25);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(71, 20);
+            this.timeLabel.Size = new System.Drawing.Size(74, 25);
             this.timeLabel.TabIndex = 2;
             this.timeLabel.Text = "00:00:00";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelTemtemHTitle
             // 
-            this.labelTemtemHTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelTemtemHTitle.AutoSize = true;
+            this.labelTemtemHTitle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelTemtemHTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTemtemHTitle.Location = new System.Drawing.Point(133, 9);
+            this.labelTemtemHTitle.Location = new System.Drawing.Point(80, 0);
+            this.labelTemtemHTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTemtemHTitle.Name = "labelTemtemHTitle";
-            this.labelTemtemHTitle.Size = new System.Drawing.Size(88, 20);
+            this.labelTemtemHTitle.Size = new System.Drawing.Size(75, 25);
             this.labelTemtemHTitle.TabIndex = 1;
             this.labelTemtemHTitle.Text = "Temtem/h";
+            this.labelTemtemHTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelTimeTitle
             // 
-            this.labelTimeTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelTimeTitle.AutoSize = true;
+            this.labelTimeTitle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelTimeTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTimeTitle.Location = new System.Drawing.Point(35, 9);
+            this.labelTimeTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTimeTitle.Margin = new System.Windows.Forms.Padding(0);
             this.labelTimeTitle.Name = "labelTimeTitle";
-            this.labelTimeTitle.Size = new System.Drawing.Size(47, 20);
+            this.labelTimeTitle.Size = new System.Drawing.Size(78, 25);
             this.labelTimeTitle.TabIndex = 0;
             this.labelTimeTitle.Text = "Time";
+            this.labelTimeTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // TimeTrackerUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TimeTrackerUI";
-            this.Size = new System.Drawing.Size(236, 77);
+            this.Size = new System.Drawing.Size(157, 50);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
